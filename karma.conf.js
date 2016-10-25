fbaf089781f7b22ce429894eca7f46c60cd7b470
andela-oesho <susan.esho@andela.com>
2016-10-25 15:35:50 +0100

    added phantom js

--- karma.conf.js
+++ karma.conf.js
@@ -28,6 +28,7 @@ module.exports = function (config) {
     plugins: [
       'karma-chrome-launcher',
       'karma-firefox-launcher',
+      'karma-phantomjs-launcher'
       'karma-jasmine',
       'karma-junit-reporter'
     ],
